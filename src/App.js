6be46e74e5a52a2aa181b36908cca996eb47b9c2
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-28 11:16:01 -0700

    [routes] create links to routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import Home from './Home'
 import Procedures from './Procerdures'
 import Contact from './Contact' 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home />
-      <Procedures />
-      <Contact />
+      <nav>
+        <Link to="/">Go Back to Home</Link> {' '}
+        <Link to="/procedures">See our Procedures</Link>{' '}
+        <Link to="/contact">Contact Us!</Link>{' '}
+      </nav>
+        <Route exact path="/" component={Home}/>
+        <Route path="/procedures" component={Procedures}/>
+        <Route path="/contact" component={Contact}/>
     </div>  
+    </Router>
   );
 }
 
