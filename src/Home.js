afbfa56c7a2bc510d79e24cac1a8d53709667b45
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-28 10:52:34 -0700

    [home] add home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Tanner's Dentistry</h1>
+                 <p>We are dentist</p>
+            </div>
+        )
+    }
+}
+
+export default Home
